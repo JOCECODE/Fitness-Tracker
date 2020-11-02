ac6865a88410952cecd2851360b5d9d7a56dcf27
JOCECODE <ulah57@gmail.com>
2020-11-01 17:07:31 -0800

    MODIFY SEEDS.JS MONGOOSE.CONNECT

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,14 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workoutdb',
+  {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
-});
+  }
+  );
 
 let workoutSeed = [
   {
